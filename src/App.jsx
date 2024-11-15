06553b576b769187ab063c931f40acf697b868e6
Ntshebetseng Moloi <nana13.moloi@gmail.com>
2024-11-15 22:06:06 +0200

    update Home and About pages with content. adding header component to home page

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,18 @@
 
-import { BrowserRouter, Routes, Route} from "react-router-dom"
-import About from "./About.jsx"
-import Home from "./Home.jsx"
+import { BrowserRouter, Link, Route, Routes } from "react-router-dom"
+import About from "../Pages/About.jsx"
+import Home from "../Pages/Home.jsx"
+import Header from "../components/Header.jsx"
 
-function App() {
-
+export default function App() {
   return (
     <BrowserRouter>
+        <Header/>
         <Routes>
           <Route path="/" element={<Home/>}/>
           <Route path="/about" element = {<About/>} />
         </Routes>
-        </BrowserRouter>
+    </BrowserRouter>
   )
 }
 
-export default App
