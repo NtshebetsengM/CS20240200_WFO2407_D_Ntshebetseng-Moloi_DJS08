a90a2c0a2f52cafba81a00459cb0198d8c8af6e9
Ntshebetseng Moloi <nana13.moloi@gmail.com>
2024-11-16 09:15:00 +0200

    refactor van filter functionality and add active styling to filter buttons

--- Pages/Vans/Vans.jsx
+++ Pages/Vans/Vans.jsx
@@ -20,7 +20,7 @@ const displayedVans = typeFilter ? vans.filter(van => van.type === typeFilter) :
 
 const vanElements = displayedVans.map(van => (
     <div key={van.id} className="van-tile">
-        <Link to= {`/vans/${van.id}`}>
+        <Link to= {van.id}>
           <img src={van.imageUrl} />
           <div className="van-info">
               <h3>{van.name}</h3>
@@ -35,12 +35,32 @@ const vanElements = displayedVans.map(van => (
       <div className="van-list-container">
         <h1>Explore our van options</h1>
         <div className="van-list-filter-buttons">
-           <Link to="?type=simple" className="van-type simple" >Simple</Link> 
-           <Link to="?type=rugged" className="van-type rugged">Rugged</Link>
-           <Link to="?type=luxury"  className="van-type luxury">Luxury</Link>
-           <Link to="." className="van-type clear-filters">Clear filter</Link>
-
+            <button  
+              onClick={() => setSearchParams({type: "rugged"})}
+              className={`van-type simple ${typeFilter === "rugged" ? "selected" : ""}`}>
+                Rugged
+            </button>
             
+            <button  
+              onClick={() => setSearchParams({type: "simple"})} 
+              className={`van-type simple ${typeFilter === "simple" ? "selected" : ""}`}>
+                Simple
+            </button> 
+           
+            <button 
+              onClick={() => setSearchParams({type: "luxury"})}
+              className={`van-type simple ${typeFilter === "luxury" ? "selected" : ""}`}>
+                Luxury
+            </button>
+           
+           {typeFilter 
+              ? (
+                <button  
+                  onClick={() => setSearchParams({})}
+                  className="van-type clear-filters">
+                    Clear filter
+                </button>)
+              : null}
         </div>
         <div className="van-list">
             {vanElements}
